e1847adc0d7b6ecddc6087077b04c9305c604448
yjfdl123 <yjfdl123@163.com>
2016-06-29 14:30:25 +0800

    test

--- multithread/thread1.py
+++ multithread/thread1.py
@@ -1,2 +1,18 @@
 #coding=utf-8
-print 1
+import threading
+from time import ctime,sleep
+def myprint():   
+    print 'start;',ctime()
+    sleep(3)
+    print 'end:',ctime()
+
+
+t= threading.Thread(target=myprint,args=())
+t.start()
+print t.isAlive()
+
+
+
+sleep(4)
+print t.isAlive()
+
