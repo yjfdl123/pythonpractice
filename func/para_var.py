7006a115dc4e4d759e24b0aa28ded223ab37e33d
yjfdl123 <yjfdl123@163.com>
2016-07-05 05:02:53 -0400

    test

--- func/para_var.py
+++ func/para_var.py
@@ -7,10 +7,14 @@ def print_para(x,y,*a,**b):
 	print 'x:',x
 	print 'y:',y
 	for item in a:
-		print item
+		print 'tuple:  ',item
 	for key in b.keys():
-		print key,b[key]
+		print 'key: ',key,b[key]
 
 
 if __name__=='__main__':
 	print_para(1,2,3,4,t=5,e=6,w=7)
+	print '\n\n'
+ 	atuple = (11,23,45)	
+	adict  = {'name':'yjf','address':'gz','tel':'133'}
+	print_para(1,2,t=5,e=6,*atuple,**adict)
