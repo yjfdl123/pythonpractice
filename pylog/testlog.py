69d89611faa3485482e67ebd87df9d9faa55daa8
yjfdl123 <yjfdl123@163.com>
2016-07-12 13:32:53 -0400

    log handler

--- pylog/testlog.py
+++ pylog/testlog.py
@@ -2,5 +2,16 @@
 
 
 import logging
-logging.basicConfig(level = logging.INFO,filename='info.log')
-logging.info('start')
+import logging.handlers
+#logging.basicConfig(level = logging.INFO,filename='info.log')
+
+myhandler = logging.StreamHandler()
+formatter = logging.Formatter('%(name)-12s: %(levelname)-8s %(message)s')
+myhandler.setFormatter(formatter)
+
+logger = logging.getLogger('yjf')
+logger.addHandler(myhandler)
+logger.setLevel(logging.DEBUG)
+
+logger.info('start')
+logger.debug('test')
